09f34b76c86181d910b6f7c291b50d0cab368bb4
Administrator <kwlsy666@naver.com>
2017-12-09 17:04:41 +0900

    2주차 예제

---
+++ funEx1.py
@@ -0,0 +1,25 @@
+import os
+
+def myaverage(a,b):
+     return (a+b)/2
+
+print(myaverage(1,5))
+
+list = [1,2,3,4,5,6]
+
+def get_max_min(a):
+    return "최대값: %d 최소값: %d" % (max(a), min(a))
+
+print(get_max_min(list))
+
+path = input("입력받은 파일의 경로: ")
+listR = list()
+def get_txt_list(path):
+    list = os.listdir()
+
+    return  list.get
+    os.get_t
+print(get_txt_list(path))
+
+
+
--- module_ex1.py
+++ module_ex1.py
@@ -1,5 +1,9 @@
 import stock
+import os
 
 print(dir(stock))
 
 print(stock.cal_upper(100))
+
+print(dir(os))
+print(os.getcwd())
